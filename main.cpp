57b7baae828e25427fac9dec5d068805b24b43ba
gismo141 <michael.r141@gmail.com>
2014-09-09 11:50:07 +0200

    Added travis-ci support

--- main.cpp
+++ main.cpp
@@ -9,19 +9,6 @@ int main(int argc, char **argv)
     QApplication::setApplicationName("binspector");
     QApplication app(argc, argv);
 
-    // STYLESHEET IS BUGGY!!!
-    /*QFile f(":qdarkstyle/style.qss");
-    if (!f.exists())
-    {
-        printf("Unable to set stylesheet, file not found\n");
-    }
-    else
-    {
-        f.open(QFile::ReadOnly | QFile::Text);
-        QTextStream ts(&f);
-        app.setStyleSheet(ts.readAll());
-    }*/
-
     view::binspector mainWindow;
     mainWindow.show();
     return app.exec();
